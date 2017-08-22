95080ad5d778490d06a390754ef66389246971be
Stephen Ball <5t3ph3n.ball@gmail.com>
2017-08-22 09:10:56 +0100

    start probabilities

--- src/app.js
+++ src/app.js
@@ -8,8 +8,7 @@ const proc = new p5(function(p){
   let steps = 16
   let grid = []
   let notes = ['C4', 'D4', 'E4', 'F4', 'G4', 'A4', 'B4', 'C5']
-  let bgColour
-  let cellColour
+  let bgColour, cellColour, highLightColour
   let margin = 10
   let isPlaying = false
   let tempo = 90
@@ -58,18 +57,28 @@ const proc = new p5(function(p){
   Tone.Transport.start()
 
   p.playStep = function(col, t){
+    let noteOptions = {}
+    let sum = 0
     for(let i = col.length - 1; i >= 0; i--){
       if(col[i] > 0){
-        const velocity = col[i]
-        synth.start(notes[i], t, 0, '8n', 0, velocity)
+        noteOptions[col[i]] = notes[i]
+        sum += col[i]
       }
     }
+    // const roll = Math.random()
+    console.log(`noteOptions is ${JSON.stringify(noteOptions)}, sum is ${sum}`)
+    // for(let i = col.length - 1; i >= 0; i--){
+    //   if(col[i] > 0){
+    //     synth.start(notes[i], t, 0, '8n', 0, 1)
+    //   }
+    // }
   }
 
   p.setup = function(){
     const cnv = p.createCanvas(800, document.documentElement.clientHeight)
     bgColour = p.color(1, 22, 39)
     cellColour = p.color(239, 255, 233)
+    highLightColour = p.color(231, 29, 54)
     p.initGrid()
   }
 
@@ -78,6 +87,7 @@ const proc = new p5(function(p){
     p.drawPlayBar()
     p.drawCells()
     p.drawPlayButton()
+    p.drawStep()
   }
 
   p.mouseReleased = function(){
@@ -102,9 +112,8 @@ const proc = new p5(function(p){
 
   p.drawPlayBar = function(){
     if(isPlaying){
-      const c = p.color(231, 29, 54)
-      p.stroke(c)
-      p.fill(c)
+      p.stroke(highLightColour)
+      p.fill(highLightColour)
       p.rect(counter * cellSz, 0, cellSz, tracks * cellSz)
     }
   }
@@ -114,7 +123,7 @@ const proc = new p5(function(p){
     p.translate(0, cellSz * tracks)
     p.noFill()
     p.stroke(cellColour)
-    p.rect(0, 0, 100, 100)
+    p.rect(0, 0, cellSz * 2, cellSz * 2)
     p.fill(cellColour)
     if(isPlaying){
       p.rect(5, 5, 90, 90)
@@ -132,10 +141,10 @@ const proc = new p5(function(p){
         p.noFill()
         p.rect(cellSz * i, cellSz * j, cellSz, cellSz)
         if(grid[i][j]){
-          let _h = grid[i][j] * cellSz
+          let _h = (1 - grid[i][j]) * cellSz
           p.noStroke()
           p.fill(cellColour)
-          p.rect(cellSz * i, (cellSz * j) + _h, cellSz, cellSz + (1 - _h))
+          p.rect(cellSz * i, (cellSz * j) + _h, cellSz, cellSz - _h)
         }
       }
     }
@@ -144,11 +153,18 @@ const proc = new p5(function(p){
 
   p.drawStep = function(){
     p.push()
-    p.translate((cellSz * steps) - 100, (cellSz * tracks) + 100)
+    p.translate(cellSz * (steps - 2), cellSz * tracks)
+    p.noFill()
+    p.stroke(cellColour)
+    p.rect(0, 0, cellSz * 2, cellSz * 2)
     p.noStroke()
     p.fill(cellColour)
-    p.textSize(72)
-    p.text(counter, 0, 0)
+    p.textSize(12)
+    p.textAlign(p.left)
+    p.text('step:', margin, p.textAscent() + margin)
+    p.textSize(48)
+    p.textAlign(p.RIGHT)
+    p.text(counter, (cellSz * 2) - margin, (cellSz * 2) - margin)
     p.pop()
   }
 
@@ -164,12 +180,13 @@ const proc = new p5(function(p){
   p.toggleGrid = function(_x, _y){
     let x = Math.floor(_x / cellSz)
     let y = Math.floor(_y / cellSz)
-    let v = (_y / cellSz) - y //get just the decimal part
+    let v = 1 - ((_y / cellSz) - y) //get just the decimal part
     if(grid[x][y]){
       grid[x][y] = 0
     } else {
       grid[x][y] = v
     }
+    console.log(grid);
   }
 
 }, 'process-container')
